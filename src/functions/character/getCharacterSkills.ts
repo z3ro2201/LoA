6e3a61a9e25a3daa0009befa09342379a64926a8
z3ro2201 <ksw1991@protonmail.ch>
2023-09-30 03:52:50 +0900

    각성기 구분 추가

--- src/functions/character/getCharacterSkills.ts
+++ src/functions/character/getCharacterSkills.ts
@@ -20,21 +20,23 @@ async function getCharacterSkillText(characterName: string) {
                 if(tmp.Tripods !== null) {
                     const tmpTripods = [];
                     for(const TripodsTmp of tmp.Tripods) {
-                        if(TripodsTmp.IsSelected === true)
+                        if(TripodsTmp.IsSelected === true) {
                             tmpTripods.push(`[${TripodsTmp.Tier} 티어] Lv ${TripodsTmp.Level}. ${TripodsTmp.Name}`);
+                        }
                     }
                     if(tmpTripods.length > 0) tripodsArr.push(tmpTripods.join('\n'));
                 }
+                const isAwakening:string = (tmp.IsAwakening === true)?'[각성기]' : '';
                 const runeData = (tmp.Rune !== null) ? `(룬: ${tmp.Rune.Name}[${tmp.Rune.Grade}])` : ``;
                 const tripods = (tripodsArr.length > 0) ? `<트라이포드>\n${tripodsArr.join('\n')}` : '';
-                const tmpData = `${tmp.Name.replace(global.regex.htmlEntity, '')} ${runeData}${tripods}`;
+                const tmpData = `${isAwakening}${tmp.Name.replace(global.regex.htmlEntity, '')} ${runeData}${tripods}`;
                 skilsArr.push(tmpData + '\n');
             }
         }
 
         // 서버 응답을 파싱하여 캐릭터 정보를 추출
         const characterTitle = (profile.Title === null) ? '' : `${profile.Title}`;     
-        const characterData = `[${profile.CharacterClassName}]\n${characterTitle} ${profile.CharacterName}\n\n[스킬정보]\n${skilsArr.join('\n')}`;
+        const characterData = `[${profile.CharacterClassName}]\n${characterTitle} ${profile.CharacterName}\n\n[스킬정보]\n${skilsArr.join('\n')}\n * 각성 스킬은 활성화 여부를 알 수 없어 기본적으로 모두 출력됩니다.`;
         return characterData;
     } catch (error) {
         throw error; // 오류를 호출자로 던짐
