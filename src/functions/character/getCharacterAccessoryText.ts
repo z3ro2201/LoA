4f589be606e82cef2a64bf0ae79cf311b0d048fa
z3ro2201 <ksw1991@protonmail.ch>
2023-09-30 04:32:05 +0900

    팔찌 설명 강제 설정

--- src/functions/character/getCharacterAccessoryText.ts
+++ src/functions/character/getCharacterAccessoryText.ts
@@ -21,14 +21,19 @@ async function getAccessoryText(characterName: string) {
                 const toolTips = tmp.Tooltip.replace(global.regex.htmlEntity, '');
                 const quality = (i < 11) ? JSON.parse(toolTips).Element_001.value.qualityValue : 0;
                 const qualityText = (i < 11) ? `(품질: ${quality})` : '';
-                const cleanedToolTipString = tmp.Tooltip.replace(/<[^>]+>/g, '').replace(/\\n/g, '');
+                const cleanedToolTipString = tmp.Tooltip;
                 const tooltipObject = JSON.parse(cleanedToolTipString);
                 let toolTipText = '';
                 for(const tmpData in tooltipObject) {
                     if(tooltipObject.hasOwnProperty(tmpData)) {
                         const element = tooltipObject[tmpData];
                         if(element && element.value && element.type && element.type.indexOf('ItemPartBox') !== -1) {
-                            toolTipText = `\n[추가효과] ${element.value.Element_001}`;
+                            if(i !== 12) toolTipText = `\n[추가효과] ${element.value.Element_001.replace(/<[^>]+>/g, '').replace(/\n/g, '')}`;
+                            else {
+                                const toolTip_bracelet = element.value.Element_001.split('<BR>');
+                                toolTipText = `\n[추가효과] ${toolTip_bracelet.join('\n').replace(global.regex.htmlEntity,'')}`;
+                                //.split('<BR>').replace(global.regex.htmlEntity, '')
+                            } 
                         }
                         if(element && element.value && element.type && element.type.indexOf('IndentStringGroup') !== -1) {
                             toolTipText = `\n${element.value.Element_000.contentStr.Element_000.contentStr.replace(global.regex.htmlEntity, '')}`;
