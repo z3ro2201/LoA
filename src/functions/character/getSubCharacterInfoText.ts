e93530fde02c68bdb48bae45742b1489dc8567f2
z3ro2201 <ksw1991@protonmail.ch>
2023-09-30 18:51:21 +0900

    캐릭터 전투레벨 출력 추가

--- src/functions/character/getSubCharacterInfoText.ts
+++ src/functions/character/getSubCharacterInfoText.ts
@@ -21,7 +21,7 @@ async function getSubCharacterInfoText(characterName: string) {
         const characterListArr = [];
         for(const tmp of data) {
             if(characterServer[0].ServerName === tmp.ServerName)
-                characterListArr.push({combatLevel: tmp.CharacterLevel, itemLevel: tmp.ItemAvgLevel, textStr: `[${tmp.CharacterClassName}] Lv ${tmp.CharacterClassName.toString().padStart(2, '0')}  ${tmp.CharacterName}  (${tmp.ItemAvgLevel})`})
+                characterListArr.push({combatLevel: tmp.CharacterLevel, itemLevel: tmp.ItemAvgLevel, textStr: `[${tmp.CharacterClassName}] Lv ${tmp.CharacterLevel}. ${tmp.CharacterClassName.toString().padStart(2, '0')}  ${tmp.CharacterName}  (${tmp.ItemAvgLevel})`})
         }
 
         // 레벨 순 정렬
