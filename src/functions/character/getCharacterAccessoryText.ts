2f720584681af3aa7265ddc192528bac08a6366c
z3ro2201 <ksw1991@protonmail.ch>
2023-09-30 22:54:11 +0900

    가독성을 위해 각인효과 제목 추가

--- src/functions/character/getCharacterAccessoryText.ts
+++ src/functions/character/getCharacterAccessoryText.ts
@@ -43,7 +43,7 @@ async function getAccessoryText(characterName: string) {
                         if (element && element.value && element.type && element.type.indexOf('IndentStringGroup') !== -1) {
                             const indentContentStr = element.value.Element_000.contentStr; // Element_006의 contentStr
                             if (indentContentStr) {
-                                toolTipText = "\n[각인효과]";
+                                toolTipText += "\n[각인효과]";
                                 Object.keys(indentContentStr).forEach(keyName => {
                                     const key = indentContentStr[keyName];
                                     if (key && key.contentStr) {
