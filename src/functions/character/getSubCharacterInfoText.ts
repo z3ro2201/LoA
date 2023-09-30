5eb621d4c02df38772cfad36924b0d64a83f5c78
z3ro2201 <ksw1991@protonmail.ch>
2023-09-30 18:35:51 +0900

    캐릭터 목록을 레벨 순으로 정렬한다.

--- src/functions/character/getSubCharacterInfoText.ts
+++ src/functions/character/getSubCharacterInfoText.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import global from '../../config/config'
 
+interface subCharacterList {
+    combatLevel: number,
+    itemLevel: number
+}
+
 async function getSubCharacterInfoText(characterName: string) {
     const apiUrl = `${global.apiUrl.lostark}characters/${characterName}/siblings`;
 
@@ -16,10 +21,24 @@ async function getSubCharacterInfoText(characterName: string) {
         const characterListArr = [];
         for(const tmp of data) {
             if(characterServer[0].ServerName === tmp.ServerName)
-                characterListArr.push(`[${tmp.CharacterClassName}] Lv ${tmp.CharacterClassName.toString().padStart(2, '0')}  ${tmp.CharacterName}  (${tmp.ItemAvgLevel})`)
+                characterListArr.push({combatLevel: tmp.CharacterLevel, itemLevel: tmp.ItemAvgLevel, textStr: `[${tmp.CharacterClassName}] Lv ${tmp.CharacterClassName.toString().padStart(2, '0')}  ${tmp.CharacterName}  (${tmp.ItemAvgLevel})`})
         }
 
-        const characterData = `[${characterServer[0].ServerName}] ${characterName}\n${characterListArr.join('\n')}\n\n총 ${characterListArr.length}개의 캐릭터 보유`;
+        // 레벨 순 정렬
+        characterListArr.sort((a:subCharacterList, b:subCharacterList) => {
+            if(a.combatLevel !== b.combatLevel) {
+                return b.combatLevel - a.combatLevel;
+            } else {
+                return b.itemLevel - a.itemLevel;
+            }
+        })
+
+        // combatLevel, itemLevel 삭제 후 textStr만 남기기
+        const sortedCharacterListArr = characterListArr.map(({textStr}) => ({textStr}));
+
+        // 정렬된 배열을 기반으로 문자열 생성 및 추가
+        const characterDataArr = sortedCharacterListArr.map(character => character.textStr);
+        const characterData = `[${characterServer[0].ServerName}] ${characterName}\n${characterDataArr.join('\n')}\n\n총 ${characterDataArr.length}개의 캐릭터 보유`;
         return characterData;
     } catch (error) {
         throw error; // 오류를 호출자로 던짐
