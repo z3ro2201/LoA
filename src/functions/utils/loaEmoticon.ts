6ae011a9d7dc7cee87394df2ba596bf8e8e80711
z3ro2201 <ksw1991@protonmail.ch>
2023-10-21 12:34:10 +0900

    로아콘 추가 및 테스트

---
+++ src/functions/utils/loaEmoticon.ts
@@ -0,0 +1,34 @@
+import global from '../../config/config'
+import { init as initDb, connect as connectDb, query as queryDb } from '../../config/mysqlConf'
+
+export const command: Record<string, string>= {
+    command: global.prefix + '~로아콘',
+    help: '로아콘 이름',
+    description: '로아콘을 보여드립니다.'
+}
+
+export const getEmoticon = async (emoticonName : string) => {
+    const loaconResult = await loaconSearch(emoticonName);
+    if(loaconResult.length === 0) {
+        return loaconResult[0];
+    } else {
+        return 204;
+    }        
+}
+
+// 이모티콘 조회, 만약 없는경우 return 0
+const loaconSearch = async (emoticonName: string) => {
+    const conn = initDb();
+    await connectDb(conn);
+    try {
+        const selectQuery = 'SELECT * FROM LOA_EMOTICON WHERE loaconName LIKE ?';
+        const selectValues = [`%${emoticonName}%`];
+        const result = await queryDb(conn, selectQuery, selectValues);
+        return result;
+    } catch (error) {
+        console.error('Query execution failed:', error);
+        return null;
+    } finally {
+        conn.end();
+    }
+}
