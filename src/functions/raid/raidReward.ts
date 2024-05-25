093282c1eded7f19f8e289ad98d233cbdcdbfeef
Z3R0 <sinuk1991@gmail.com>
2024-05-25 11:28:35 +0900

    레이드 보상 초성 변경

--- src/functions/raid/raidReward.ts
+++ src/functions/raid/raidReward.ts
@@ -3,7 +3,7 @@ import global from '../../config/config'
 import { init as initDb, connect as connectDb, query as queryDb } from '../../config/mysqlConf'
 
 const command: Record<string, string>= {
-    command: global.prefix + '보상(ㅂㅅ)',
+    command: global.prefix + '보상(ㄹㅇㄷㅂㅅ)',
     help: '[오레하|아르고스|발탄노말|발탄하드|비아노말|비아하드|쿠크|노브|하브|노칸|하칸|노양겔|하양겔|상노탑|상하탑|노멘|하멘|에키드나노말|에키드나하드]\n(재료를 보려면 뒤에 "더보기" 추가하세요. 초성가능)',
     description: '레이드 보상을 볼 수 있습니다.'
 }
