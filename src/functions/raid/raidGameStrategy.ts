97d0e7616ea07bec92a76d0b1e0dc125be3699d6
z3ro2201 <z3ro2201@gmail.com>
2024-06-08 14:16:59 +0900

    패치 및 쿠폰, 공략정보 수정

---
+++ src/functions/raid/raidGameStrategy.ts
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import Cheerio from 'cheerio';
+import { init as initDb, connect as connectDb, query as queryDb } from '../../config/mysqlConf'
+
+export const raidGameList = async () => {
+    
+    try {
+
+
+        const lostarkRaidData = await raidList();
+
+        if(lostarkRaidData.length === 0) {
+            return '일치하는 정보가 없습니다.'
+        } else {
+            const raidListArr = [];
+            lostarkRaidData.forEach(item => {
+                raidListArr.push(item.boss_name);
+            })
+            return `[레이드 정보] /공략 [${raidListArr.join('|')}]`
+        }
+    } catch (error) {
+        console.error('Error fetching patch news:', error);
+        return [];
+    }
+};
+
+export const raidGameStrategy = async (raidName : string) => {
+    
+    try {
+        const lostarkRaidData = await raidSearch(raidName);
+
+        if(lostarkRaidData.length === 0) {
+            return '일치하는 정보가 없습니다.'
+        } else {
+            let bossname = lostarkRaidData[0].boss_name;
+            const raidListArr = [];
+            lostarkRaidData.forEach(item => {
+                console.log(item)
+                raidListArr.push(`${item.boss_phase !== '' && item.boss_phase !== null ? ` [${item.boss_phase}] `:''}${item.boss_url}`)
+            })
+            return `[${bossname}]\n${raidListArr.join('\n')}`
+        }
+    } catch (error) {
+        console.error('Error fetching patch news:', error);
+        return [];
+    }
+};
+
+
+// 쿠폰 조회, 만약 없는경우 return 0
+const raidSearch = async (raidName: string) => {
+    const conn = initDb();
+    await connectDb(conn);
+    try {
+        const selectQuery = 'SELECT * FROM LOA_GAMESTRATEGY_INFO WHERE boss_raidname = ? OR boss_name = ?';
+        const selectValues = [raidName, raidName];
+        const result = await queryDb(conn, selectQuery, selectValues);
+        return result;
+    } catch (error) {
+        console.error('Query execution failed:', error);
+        return null;
+    } finally {
+        conn.end();
+    }
+}
+
+// 쿠폰 조회, 만약 없는경우 return 0
+const raidList = async () => {
+    const conn = initDb();
+    await connectDb(conn);
+    try {
+        const selectQuery = 'SELECT * FROM LOA_GAMESTRATEGY_INFO GROUP BY boss_name';
+        const result = await queryDb(conn, selectQuery);
+        return result;
+    } catch (error) {
+        console.error('Query execution failed:', error);
+        return null;
+    } finally {
+        conn.end();
+    }
+}
--- src/routes/raidRoutes.ts
+++ src/routes/raidRoutes.ts
@@ -2,23 +2,42 @@ import express, { Router, Request, Response } from 'express';
 
 // 각종 명령어 목록
 import raidReward, { raidName } from '../functions/raid/raidReward'
+import {raidGameList, raidGameStrategy} from '../functions/raid/raidGameStrategy'
 
 const raidRouter: Router = express.Router();
 
 // 안내메시지
-raidRouter.get('/', (req: Request, res: Response) => {
+raidRouter.get('/reward/', (req: Request, res: Response) => {
     const helpMessage = raidReward();
     res.status(200).json({
         code: 200,
         message: helpMessage
     });
-    console.log(raidReward)
 });
 
-raidRouter.get('/:raidNameStr/:isGold?', async (req: Request, res: Response) => {
+raidRouter.get('/reward/:raidNameStr/:isGold?', async (req: Request, res: Response) => {
     const raidValue = req.params.raidNameStr;
     const goldValue = req.params.isGold || 0;
     const RaidData = await raidName(raidValue, goldValue);
+    res.status(200).json({
+        code: 200,
+        message: RaidData
+    });
+});
+
+// 레이드 컨닝페이퍼
+raidRouter.get('/GameStrategy/', async (req: Request, res: Response) => {
+    const RaidData = await raidGameList();
+    res.status(200).json({
+        code: 200,
+        message: RaidData
+    });
+});
+
+// 레이드 컨닝페이퍼
+raidRouter.get('/GameStrategy/:raidNameStr', async (req: Request, res: Response) => {
+    const raidValue = req.params.raidNameStr;
+    const RaidData = await raidGameStrategy(raidValue);
     res.status(200).json({
         code: 200,
         message: RaidData
