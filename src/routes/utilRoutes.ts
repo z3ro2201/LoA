2e37ba6971fe6a00a629752cf54a64c595cea40f
z3r02201 <sinuk1991@naver.com>
2024-09-04 21:48:16 +0900

    return

--- src/routes/utilRoutes.ts
+++ src/routes/utilRoutes.ts
@@ -202,10 +202,10 @@ utilRouter.get(['/exchange', '/exchange/:exchange', '/exchange/:exchange/:rate']
 // 코구
 utilRouter.get('/kborank', async (req: Request, res: Response) => {
     const getRankData = await getKBORankData();
-    const { code, message } = getRankData
+    // const { code, message } = getRankData
     res.status(200).json({
-        code: code,
-        data: message
+        code: getRankData.code,
+        data: getRankData.message
     })
 })
 
@@ -221,20 +221,18 @@ utilRouter.get('/kboscore/:teamName', async (req: Request, res: Response) => {
         })
     }
     const getKBOMatchScoreData = await getKBOMatchScores(teamName);
-    const { code, message } = getKBOMatchScoreData
     res.status(200).json({
-        code: code,
-        data: message
+        code: getKBOMatchScoreData.code,
+        data: getKBOMatchScoreData.message
     })
 })
 
 // 코구 현재(오늘) 경기스코어정보
 utilRouter.get('/kbonow', async (req: Request, res: Response) => {
     const getKBOMatchScoreData = await getKBONowMatchScore();
-    const { code, message } = getKBOMatchScoreData
     res.status(200).json({
-        code: code,
-        data: message
+        code: getKBOMatchScoreData.code,
+        data: getKBOMatchScoreData.message
     })
 })
 
